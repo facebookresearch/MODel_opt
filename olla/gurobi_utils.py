4548b3cab332aebc06843a80db069793e840445d
jacobkahn <jacobkahn1@gmail.com>
2022-12-28 14:22:13 -0800

    Add interface for initializing Gurobi env

---
+++ olla/gurobi_utils.py
@@ -0,0 +1,28 @@
+import gurobipy
+import os
+import logging
+
+
+def get_gurobi_env():
+    try:
+        # Try using an ISV license based on the supplied environment variables
+        env = gurobipy.Env.OtherEnv(
+            os.getenv("OLLA_GUROBI_LOGFILE", ""),
+            os.getenv("OLLA_GUROBI_ISV_NAME"),
+            os.getenv("OLLA_GUROBI_ISV_APP_NAME"),
+            int(os.getenv("OLLA_GUROBI_ISV_EXPIRATION")),
+            os.getenv("OLLA_GUROBI_ISV_CODE"),
+        )
+        logging.info("Successfully created Gurobi env with ISV license")
+    except:
+        logging.warning(
+            "Failed to create env based on ISV license. If you intended to use",
+            " an ISV license, set the OLLA_GUROBI_ISV_NAME",
+            " OLLA_GUROBI_ISV_APP_NAME, OLLA_GUROBI_ISV_EXPIRATION,",
+            " and OLLA_GUROBI_ISV_CODE to their corresponding values."
+            " Falling back to default Gurobi environment initialization.",
+        )
+        # Fall back to default env
+        env = gurobipy.Env()
+
+    return env
