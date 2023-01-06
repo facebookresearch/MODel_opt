bef07aba48b87231638a334f400459ead0bde137
Mostafa Elhoushi <melhoushi@fb.com>
2023-01-06 00:49:12 -0500

    Call optimizer.zero_grad() when profiling

--- olla/torch/torch_graph_importer.py
+++ olla/torch/torch_graph_importer.py
@@ -194,6 +194,8 @@ class TorchGraphImporter:
             if not isinstance(args, Iterable):
                 args = [args]
             params_and_buffers = {**params, **buffers}
+            if optimizer is not None and is instance(optimizer, torch.optim.Optimizer):
+                optimizer.zero_grad()
             out = stateless.functional_call(model, params_and_buffers, args)
             if mode == "eval":
                 return out
