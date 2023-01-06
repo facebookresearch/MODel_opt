84c070ab4fac2052d5ed9140aea228dec8eb4696
Mostafa Elhoushi <melhoushi@meta.com>
2023-01-06 14:10:22 -0800

    add simpler example

--- tests/olla_apis_test.py
+++ tests/olla_apis_test.py
@@ -5,6 +5,30 @@ import unittest
 import olla
 
 class OLLAAPIsTest(unittest.TestCase):
+    def testSimpleTrain(self):
+        class SimpleModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.param = torch.nn.Parameter(torch.randn((1,)), requires_grad=True)
+
+            def forward(self, x):
+                return x + self.param + 5
+
+        model = SimpleModule()
+        input = torch.randn((1), requires_grad = True)
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+        loss_fn = torch.nn.MSELoss()
+
+        model_opt = olla.optimize(model, input, optimizer=optimizer, loss_fn=loss_fn)
+
+        y_orig = model(input)
+        y = model_opt(input)
+        assert(torch.allclose(y, y_orig))
+
+        y2 = model_opt(input)
+        # everytime we run the model, the weights get updated, so we expect the output to be different
+        assert(not torch.allclose(y, y2))
+
     def testAlexNetTrain(self):
         model = torchvision.models.alexnet()
         input = torch.randn((32, 3, 224, 224), requires_grad = True)
