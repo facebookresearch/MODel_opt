bd71d7e18e31048857c8523d36269ad7fec5cd97
Mostafa Elhoushi <melhoushi@meta.com>
2023-01-21 12:34:37 -0800

    add copyright header

--- olla/ilp_solver.py
+++ olla/ilp_solver.py
@@ -1,3 +1,9 @@
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import sys
 import logging
 from typing import Any, Callable, Dict, List, Optional, Union
