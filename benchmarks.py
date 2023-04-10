6c0e8d0b3347086847e5ed7c405b5cbcf4b2dc35
Mostafa Elhoushi <melhoushi@meta.com>
2023-04-10 14:17:14 -0700

    add mobilenet

--- benchmarks.py
+++ benchmarks.py
@@ -115,6 +115,58 @@ class Benchmark:
         elif model_name == "mobilenet":
             model = torchvision.models.mobilenet_v2()
             inputs = (torch.randn((batch_size, 3, 224, 224)),)
+        elif model_name == "mobilenetv1":
+            import torch.nn as nn
+            class MobileNetV1(torch.nn.Module):
+                def __init__(self, ch_in, n_classes):
+                    super(MobileNetV1, self).__init__()
+
+                    def conv_bn(inp, oup, stride):
+                        return nn.Sequential(
+                            nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+                            nn.BatchNorm2d(oup),
+                            nn.ReLU(inplace=True)
+                            )
+
+                    def conv_dw(inp, oup, stride):
+                        return nn.Sequential(
+                            # dw
+                            nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                            nn.BatchNorm2d(inp),
+                            nn.ReLU(inplace=True),
+
+                            # pw
+                            nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                            nn.BatchNorm2d(oup),
+                            nn.ReLU(inplace=True),
+                            )
+
+                    self.model = nn.Sequential(
+                        conv_bn(ch_in, 32, 2),
+                        conv_dw(32, 64, 1),
+                        conv_dw(64, 128, 2),
+                        conv_dw(128, 128, 1),
+                        conv_dw(128, 256, 2),
+                        conv_dw(256, 256, 1),
+                        conv_dw(256, 512, 2),
+                        conv_dw(512, 512, 1),
+                        conv_dw(512, 512, 1),
+                        conv_dw(512, 512, 1),
+                        conv_dw(512, 512, 1),
+                        conv_dw(512, 512, 1),
+                        conv_dw(512, 1024, 2),
+                        conv_dw(1024, 1024, 1),
+                        nn.AdaptiveAvgPool2d(1)
+                    )
+                    self.fc = nn.Linear(1024, n_classes)
+
+                def forward(self, x):
+                    x = self.model(x)
+                    x = x.view(-1, 1024)
+                    x = self.fc(x)
+                    return x
+            model = MobileNetV1(ch_in=3, n_classes=1000)
+            inputs = (torch.randn((batch_size, 3, 224, 224)),)
         elif model_name == "raft":
             model = torchvision.models.optical_flow.raft_small()
             inputs = (
